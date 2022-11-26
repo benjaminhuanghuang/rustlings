e27ae90b7f25d96c28c281255bf5fdc46986f444
Benjamin Huang <benjhuang@gmail.com>
2022-11-25 17:42:48 -0800

    options

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -16,9 +16,8 @@
 // - The input is going to be a Vector of a 2-length tuple,
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a Vector of strings.
-// No hints this time!
+// Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -30,20 +29,47 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(to_uppercase(&string)),
+                Command::Trim => output.push(trim(&string)),
+                // I get a mismatched type error here
+                Command::Append(n) => output.push(append_bar(&string, n)),
+            }
         }
         output
+    }
+
+    
+    fn to_uppercase(string: &str) -> String {
+        string.to_uppercase()
+    }
+
+    fn trim(string: &str) -> String {
+        string.trim().to_string()
+    }
+
+    fn append_bar2(string: &str, count: &usize) -> String {
+        string.to_string() +&"bar".repeat(*count)
+    }
+
+    fn append_bar(string: &str, count: &usize) -> String {
+        let mut result = String::from(string);
+        for _ in 0..*count {
+            result += "bar";
+        }
+        result
     }
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use my_module::transformer;
     use super::Command;
 
     #[test]
