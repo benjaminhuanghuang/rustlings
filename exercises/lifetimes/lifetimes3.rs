d021d27d8542965834a8b9509092ed4cdc620843
Benjamin Huang <benjhuang@gmail.com>
2022-11-26 18:12:28 -0800

    update

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,12 +4,11 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
+
 
 fn main() {
     let name = String::from("Jill Smith");
